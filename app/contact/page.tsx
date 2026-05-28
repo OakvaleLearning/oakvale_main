d32a158f89724158852a2429f025b3e527fbefcd
Victor Olorunda <victorolorunda6@gmail.com>
2026-05-28 12:45:13 +0100

    new page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,5 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
 import ContactPage from '../components/ContactPage';
 
 export default function Contact() {
-  const router = useRouter();
-  
-  const navigate = (page: string) => {
-    if (page === 'home') {
-      router.push('/');
-    } else {
-      router.push(`/${page}`);
-    }
-  };
-
-  return <ContactPage onNavigate={navigate} />;
+  return <ContactPage />;
 }
